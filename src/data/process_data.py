39479fcd5ff810eb84472dcbb9b97d7ca65b81fe
TimWeichsel <weichselberger.tim@icloud.com>
2025-09-26 14:13:15 +0200

    reduce_to_columns added

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-def delete_columns (df: pd.DataFrame, columns_to_delete: list) ->  pd.DataFrame:
-    return df.drop
+def reduce_to_columns (df: pd.DataFrame, keep_columns: list) -> pd.DataFrame:
+    missing_columns = [column for column in keep_columns if column not in df]
+    if missing_columns:
+        raise ValueError (f"Columns not known: {missing_columns}")
+    return df[keep_columns]
