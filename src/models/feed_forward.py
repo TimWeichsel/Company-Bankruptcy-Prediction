da06cc7d4c5d0afc099bb36b8caed8943bc9fd4f
TimWeichsel <weichselberger.tim@icloud.com>
2025-10-24 19:19:46 +0200

    ff updated

--- src/models/feed_forward.py
+++ src/models/feed_forward.py
@@ -2,13 +2,14 @@ import torch
 import torch.nn as nn
 
 class FeedForwardNN (nn.Module):
-    def __init__(self, hidden_layer_dimension, in_dim, drop_out_rate=0.3, activation_func=nn.ReLU()):
+    def __init__(self, in_dim, hidden_layer_dimension = (64, 128), dropout_rate=0.3, activation_func_class=nn.ReLU):
         super().__init__()
         layers = []
         current_in_dim = in_dim
         for current_out_dim in hidden_layer_dimension:
-            layers.append(nn.Linear(current_in_dim,current_out_dim),activation_func, nn.Dropout(drop_out_rate)) #Adding all layers except last one
-        layers.append(nn.Linear(current_out_dim,1))
+            layers += [nn.Linear(current_in_dim,current_out_dim),activation_func_class(), nn.Dropout(dropout_rate)] #Adding all layers except last one
+            current_in_dim = current_out_dim
+        layers += [nn.Linear(current_in_dim,1)] #Adding Last layer
         self.layer_container = nn.Sequential(*layers)
         
     def forward(self, X): 
